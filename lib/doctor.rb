67486b6d19c3db5e406fd06b3c28a43a6d4e4988
Christian V <viray.christianj@gmail.com>
2019-04-08 23:21:48 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -18,9 +18,10 @@ class Doctor
   end 
   
   
-def new_appointment(patient, date)
+  def new_appointment(patient, date)
     Appointment.new(patient, date, self)
   end
+  
 
   def appointments
     Appointment.all.select do |appointment|
@@ -28,10 +29,12 @@ def new_appointment(patient, date)
     end
   end
 
+
   def patients
     appointments.collect do |appointment|
       appointment.patient
     end
   end
+  
   
 end 
