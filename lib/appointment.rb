f4df564e86c5742887bbefe3e5ab54ce77a8ecb4
Christian V <viray.christianj@gmail.com>
2019-04-08 23:34:30 +0000

    Automatically backed up by Learn

--- lib/appointment.rb
+++ lib/appointment.rb
@@ -1,23 +1,16 @@
-class Appointment
-  
-  
-  attr_accessor :date, :patient, :doctor
-  
-  
+class Appointment 
+  attr_accessor  :date, :patient, :doctor 
+
   @@all = []
-  
-  
+
   def self.all 
-    @@all 
-  end 
-  
-  
-  def initialize(date, patient, doctor)
-    @date = date 
+    @@all
+  end
+
+  def initialize(patient, date, doctor)
+    @date = date
     @patient = patient
-    @doctor = doctor 
-    @@all << self 
-  end 
-  
-  
+    @doctor = doctor
+    @@all << self
+  end
 end 
