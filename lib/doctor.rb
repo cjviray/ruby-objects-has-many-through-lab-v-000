e813fd49cb726e8e45003bfa6983007a55c26361
Christian V <viray.christianj@gmail.com>
2019-04-08 23:33:58 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -1,27 +1,20 @@
-class Doctor
-  
-  
+class Doctor 
   attr_accessor :name
 
-
   @@all = []
-  
-  
+
+  def self.all
+    @@all
+  end
+
   def initialize(name)
     @name = name
     @@all << self
   end
-  
- 
-  def self.all
-    @@all 
-  end 
-  
-  
+
   def new_appointment(patient, date)
     Appointment.new(patient, date, self)
   end
-  
 
   def appointments
     Appointment.all.select do |appointment|
@@ -29,12 +22,9 @@ class Doctor
     end
   end
 
-
   def patients
     appointments.collect do |appointment|
       appointment.patient
     end
   end
-  
-  
 end 
